b6e0a9d2add33a8e0910f3572fcce6adb71497ba
sooraj10 <37476049+sooraj10@users.noreply.github.com>
2020-04-20 02:24:48 +0530

    Update Redis.java

--- src/Main/Redis.java
+++ src/Main/Redis.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Main;
 
 /**
  *
- * @author hp
+ * @author sooraj10
  */
 import java.util.*;
 interface Redis {
